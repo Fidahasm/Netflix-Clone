eada0783bbcbb01a6ecd37c45494b59a68099bea
Fidahasm <fidahkt@gmail.com>
2024-08-02 23:14:37 +0530

    UI updated

--- netflix_main/src/App.js
+++ netflix_main/src/App.js
@@ -1,10 +1,12 @@
 import '.'
+import Banner from './components/Banner/Banner';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
     </div>
   );
 }
---
+++ netflix_main/src/components/Banner/Banner.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import '../Banner/Banner.css'
+
+function Banner() {
+  return (
+    <div>
+        <div  className='banner'>
+            <div className='banner-content'>
+            <h1>Movie Name</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, vitae. Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, vitae. </p>
+            <div className='buttons'>
+                <div className='btn' id='play-btn'>
+                    <i class='bx bx-play'>Play</i>
+                   
+                    </div>
+                <div className='btn' id='list-btn'>
+                    <i class='bx bx-plus'  >My List</i>
+                
+                    </div>
+            </div>
+            </div>
+            </div>
+        
+      
+    </div>
+  )
+}
+
+export default Banner
